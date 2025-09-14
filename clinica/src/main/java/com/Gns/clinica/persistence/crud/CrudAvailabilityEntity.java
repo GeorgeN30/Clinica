7115fe6b8b2f7711d0b6d056d9ae47f1db5c7118
GeorgeNs30 <george.villachica@gmail.com>
2025-09-14 18:28:27 -0500

    add availability features

--- clinica/src/main/java/com/Gns/clinica/persistence/crud/CrudAvailabilityEntity.java
+++ clinica/src/main/java/com/Gns/clinica/persistence/crud/CrudAvailabilityEntity.java
@@ -1,7 +1,15 @@
 package com.Gns.clinica.persistence.crud;
 
+import com.Gns.clinica.domain.dto.request.AvailabilityRequestDto;
+import com.Gns.clinica.domain.enums.AvailabilityStatus;
 import com.Gns.clinica.persistence.entity.AvailabilityEntity;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface CrudAvailabilityEntity extends ListCrudRepository<AvailabilityEntity, Long> {
+    AvailabilityEntity findByDateAndStatus(LocalDate date, AvailabilityStatus availabilityStatus);
+    List<AvailabilityEntity> findByDateBetweenAndStatus(LocalDate date, LocalDate date2, AvailabilityStatus status);
+
 }

2c6e59f364645e3d61ac42471fdc466c9da7000e
GeorgeNs30 <george.villachica@gmail.com>
2025-09-21 20:05:30 -0500

    add validation

--- clinica/src/main/java/com/Gns/clinica/domain/dto/request/AvailabilityRequestDto.java
+++ clinica/src/main/java/com/Gns/clinica/domain/dto/request/AvailabilityRequestDto.java
@@ -1,13 +1,26 @@
 package com.Gns.clinica.domain.dto.request;
 import com.Gns.clinica.domain.enums.AvailabilityStatus;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public record AvailabilityRequestDto(
+        @NotNull(message = "Doctor ID cannot be null")
         Long idDoctor,
+
+        @NotNull(message = "Date cannot be null")
+        @FutureOrPresent(message = "Date cannot be in the past")
         LocalDate date,
+
+        @NotNull(message = "Start time cannot be null")
         LocalTime startTime,
+
+        @NotNull(message = "End time cannot be null")
         LocalTime endTime,
+
+        @NotNull(message = "Status cannot be null")
         AvailabilityStatus status
 ) {
 }

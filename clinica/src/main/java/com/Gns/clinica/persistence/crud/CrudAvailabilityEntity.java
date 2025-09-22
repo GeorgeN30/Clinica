844fbce531f15bd491b725a740b8f390bf0b82d8
GeorgeNs30 <george.villachica@gmail.com>
2025-09-21 20:05:44 -0500

    fix crud availability

--- clinica/src/main/java/com/Gns/clinica/persistence/crud/CrudAvailabilityEntity.java
+++ clinica/src/main/java/com/Gns/clinica/persistence/crud/CrudAvailabilityEntity.java
@@ -1,15 +1,14 @@
 package com.Gns.clinica.persistence.crud;
 
-import com.Gns.clinica.domain.dto.request.AvailabilityRequestDto;
 import com.Gns.clinica.domain.enums.AvailabilityStatus;
 import com.Gns.clinica.persistence.entity.AvailabilityEntity;
 import org.springframework.data.repository.ListCrudRepository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface CrudAvailabilityEntity extends ListCrudRepository<AvailabilityEntity, Long> {
-    AvailabilityEntity findByDateAndStatus(LocalDate date, AvailabilityStatus availabilityStatus);
+    Optional<AvailabilityEntity> findByDateAndStatus(LocalDate date, AvailabilityStatus availabilityStatus);
     List<AvailabilityEntity> findByDateBetweenAndStatus(LocalDate date, LocalDate date2, AvailabilityStatus status);
-
 }

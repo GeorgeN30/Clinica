5e7dd28987f4097d97d9bd23759e2b129ebbf11a
GeorgeNs30 <george.villachica@gmail.com>
2025-10-01 20:32:08 -0500

    ad @NonNull

--- clinica/src/main/java/com/Gns/clinica/security/config/JwtFilter.java
+++ clinica/src/main/java/com/Gns/clinica/security/config/JwtFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -30,8 +31,8 @@ public class JwtFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
         String authHeader = request.getHeader(AUTHORIZATION);
 
         if(authHeader == null || !authHeader.startsWith("Bearer ")) {
